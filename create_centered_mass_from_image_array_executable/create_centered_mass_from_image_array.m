bc7ad0a897d4e4956ddb46eb0d249d1219c0b22a
BJ Anderson <andersonion@users.noreply.github.com>
2019-07-08 14:06:13 -0400

    Fixed load_niigz error.

--- create_centered_mass_from_image_array_executable/create_centered_mass_from_image_array.m
+++ create_centered_mass_from_image_array_executable/create_centered_mass_from_image_array.m
@@ -2,7 +2,14 @@ function create_centered_mass_from_image_array(in_file,out_file)
 %in_file = '/glusterspace/VBM_14obrien01_DTI101b-work/preprocess/base_images/native_reference_controlSpring2013_4.nii.gz';
 %out_file = '/glusterspace/VBM_14obrien01_DTI101b-work/preprocess/base_images/native_reference_controlSpring2013_4_TEST.nii.gz';
 
-if exist('wasteful_load','var')
+% 8 July 2019, BJA: changing from dependency on 'wasteful_load' to
+% try/catch block which uses load_niigz_hdr IF AVAILABLE
+try
+    nii.hdr=load_niigz_hdr(in_file);
+    image=zeros(nii.hdr.dime.dim(2:4),nifti1('data_type',nii.hdr.dime.datatype));
+catch
+    wasteful_load=1;
+%if exist('wasteful_load','var')
     nii = load_untouch_nii(in_file);
     % testing of load_niigz shows its a bit slower than the other code. That is
     % confusing. Maybe the true savings of load_niigz only show up for mask
@@ -12,9 +19,9 @@ if exist('wasteful_load','var')
     % On paying closer attention to what this function is doing, There is
     % no reason to load the image data.... Hence wastful_load disables
     % load/save_untouch code. 
-else
-    nii.hdr=load_niigz_hdr(in_file);
-    image=zeros(nii.hdr.dime.dim(2:4),nifti1('data_type',nii.hdr.dime.datatype));
+%else
+%    nii.hdr=load_niigz_hdr(in_file);
+%    image=zeros(nii.hdr.dime.dim(2:4),nifti1('data_type',nii.hdr.dime.datatype));
 end
 
 dims=nii.hdr.dime.dim(2:4);
