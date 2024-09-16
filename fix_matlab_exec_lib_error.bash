9982bef787912d842412a0d8b20aa2c8402ec0c7
rja <rja@kea.dhe.duke.edu>
2024-09-16 14:38:08 -0400

    Updated library fixing script to be more robust.

--- fix_matlab_exec_lib_error.bash
+++ fix_matlab_exec_lib_error.bash
@@ -14,8 +14,16 @@ if [[ -e ${source} ]];then
     sudo ln -s ${source} ${lib_B};
     fi
     
-    echo "Finsihed attempt to fix missing library; please try running your code again to see if error persists."
+    echo "Finished attempt to fix missing library; please try running your code again to see if error persists."
 else
-    echo "Unable to locate the appropriate file to link to; unsure of how to fixt your error. A thousand pardons...";
+    echo "Unable to locate the appropriate file to link to; unsure of how to fix your error. A thousand pardons...";
     echo "File in questions: ${source}."
+    echo "Perhaps try the command 'sudo apt-get install libncurses5-dev libncursesw5-dev' ";
+fi
+
+x_lib=${lib_dir}libXt.so.6;
+if [[ ! -e ${x_lib} ]];then
+    echo "libXt.so.6 not found, trying to install that library now..."
+    echo 'sudo apt-get install libxt6';
+    sudo apt-get install libxt6;
 fi
