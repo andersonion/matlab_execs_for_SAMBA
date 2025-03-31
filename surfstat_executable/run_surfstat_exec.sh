1af6677f6a77445fb9bf0204c11f16162691e01d
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2025-03-31 15:13:18 -0400

    Fixing java.opts copy lines

--- surfstat_executable/run_surfstat_exec.sh
+++ surfstat_executable/run_surfstat_exec.sh
@@ -27,7 +27,7 @@ else
       args="${args} \"${token}\"" 
       shift
   done
-  cp -n ${exe_dir}/java.opts .
+  cp -n ${exe_dir}/*java.opts ./java.opts
   eval "\"${exe_dir}/surfstat_exec\"" $args
   ret_code=$?
 
